0bba5afccd507b1e93bfcf4e30af2a0ebd491be1
TurboFen <turbofenix@yandex.ru>
2020-01-03 22:25:42 +0300

    4 push

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,6 +4,8 @@
 #include <header.hpp>
 
 TEST(TestProg, EmptyTest) {
-  char* argv[] = {"../misc/ftp"};
+  char* argv[] = {"Test"};
   EXPECT_EQ(main_special(1, argv[]), 0);
+  char* argv[] = {"Test", "../misc/ftp"};
+  EXPECT_EQ(main_special(2, argv[]), 0);
 }
